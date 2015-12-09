5c5bcf6485fcc818099a9420f1394055575952b7
rrosatti <rodrigo_rosatti@hotmail.com>
2015-12-09 16:31:10 +0000

    users and sessions controllers were configured. User creation and authentication were configured

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   
   root 'posts#index'
   
+  get 'login' => 'sessions#new', as: 'login'
+  get 'logout' => 'sessions#destroy', as: 'logout'
+  
   resources 'posts'
   resources 'sessions'
   resources 'users'
