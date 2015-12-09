873d03e3c42c8c76bb2a10c10f544620f750d5c2
rrosatti <rodrigo_rosatti@hotmail.com>
2015-12-09 16:12:35 +0000

    edit, index, new and show views were created. Routes were created.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,23 @@
 class PostsController < ApplicationController
+  
+  before_action :find_post, only: [:show, :edit, :destroy, :update]
+  
   def index
+    @posts = Post.all.order("created_at DESC")
   end
 
   def new
+    @post = Post.new
+  end
+  
+  def create
+    @post = Post.new(post_params)
+    
+    if @post.save 
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def show
@@ -10,4 +25,27 @@ class PostsController < ApplicationController
 
   def edit
   end
+  
+  def update
+    if @post.update(post_params)
+      redirect_to @post
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @post.destroy
+    redirect_to root_path
+  end
+  
+  private
+  
+    def find_post
+      @post = Post.find(params[:id])
+    end
+  
+    def post_params
+      params.require(:post).permit(:title, :body)
+    end
 end
